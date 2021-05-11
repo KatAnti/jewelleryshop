420a30ce1a13f125a2eaad0a8536f35e9bb87600
Kat <ekatanti@yandex.ru>
2021-05-11 20:53:23 +0300

    Завершает верстку лавной страницы

--- build/js/main.js
+++ build/js/main.js
@@ -66,3 +66,25 @@
 
   swiper.init();
 })();
+
+'use strict';
+
+(function () {
+  var tabs = document.querySelectorAll('.tab');
+  var tabsContents = document.querySelectorAll('.tab__content');
+
+  tabsContents.forEach(function (tab) {
+    tab.classList.add('tab__content--hide');
+  });
+
+  if (tabs.length > 0) {
+    tabs.forEach(function (tab) {
+      tab.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        tab.classList.toggle('tab--open');
+        tab.parentNode.querySelector('.tab__content').classList.toggle('tab__content--hide');
+      });
+    });
+  }
+
+})();
