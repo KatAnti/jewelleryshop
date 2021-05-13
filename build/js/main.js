408d51d7cdac9da38c61d9fa3a6dbe279c4f7cc5
Kat <ekatanti@yandex.ru>
2021-05-13 20:31:28 +0300

    Добавляет верстку детальной страницы

--- build/js/main.js
+++ build/js/main.js
@@ -61,6 +61,8 @@
 'use strict';
 
 (function () {
+  var mobileWidth = window.matchMedia('(max-width: 768px)');
+
   var swiper = new window.Swiper('.swiper-container', {
     direction: 'horizontal',
     slidesPerView: 2,
@@ -71,6 +73,7 @@
     loopFillGroupWithBlank: true,
     pagination: {
       el: '.swiper-pagination',
+      type: 'bullets',
       clickable: true,
       renderBullet: function (index, className) {
         return '<span class="' + className + '">' + (index + 1) + '</span>';
@@ -84,11 +87,34 @@
       1024: {
         slidesPerView: 4,
         slidesPerGroup: 4,
-      }
+      },
     },
   });
 
+  var setSlidesCounter = function () {
+    var bullets = Array.from(document.querySelectorAll('.swiper-pagination-bullet'));
+    var currentDots = document.querySelector('.swiper-count-mobile__current');
+    var totalDots = document.querySelector('.swiper-count-mobile__total');
+    var countOfActiveBullet = bullets.findIndex(function (item) {
+      return item.classList.contains('swiper-pagination-bullet-active');
+    }) + 1;
+
+
+    totalDots.textContent = bullets.length;
+    currentDots.textContent = countOfActiveBullet;
+  };
+
   swiper.init();
+  setSlidesCounter();
+
+  mobileWidth.addEventListener('change', function () {
+    setSlidesCounter();
+  });
+
+  swiper.on('slideChangeTransitionEnd', function () {
+    setSlidesCounter();
+  });
+
 })();
 
 'use strict';
--- source/js/swiper.js
+++ source/js/swiper.js
@@ -1,6 +1,8 @@
 'use strict';
 
 (function () {
+  var mobileWidth = window.matchMedia('(max-width: 768px)');
+
   var swiper = new window.Swiper('.swiper-container', {
     direction: 'horizontal',
     slidesPerView: 2,
@@ -11,6 +13,7 @@
     loopFillGroupWithBlank: true,
     pagination: {
       el: '.swiper-pagination',
+      type: 'bullets',
       clickable: true,
       renderBullet: function (index, className) {
         return '<span class="' + className + '">' + (index + 1) + '</span>';
@@ -24,9 +27,32 @@
       1024: {
         slidesPerView: 4,
         slidesPerGroup: 4,
-      }
+      },
     },
   });
 
+  var setSlidesCounter = function () {
+    var bullets = Array.from(document.querySelectorAll('.swiper-pagination-bullet'));
+    var currentDots = document.querySelector('.swiper-count-mobile__current');
+    var totalDots = document.querySelector('.swiper-count-mobile__total');
+    var countOfActiveBullet = bullets.findIndex(function (item) {
+      return item.classList.contains('swiper-pagination-bullet-active');
+    }) + 1;
+
+
+    totalDots.textContent = bullets.length;
+    currentDots.textContent = countOfActiveBullet;
+  };
+
   swiper.init();
+  setSlidesCounter();
+
+  mobileWidth.addEventListener('change', function () {
+    setSlidesCounter();
+  });
+
+  swiper.on('slideChangeTransitionEnd', function () {
+    setSlidesCounter();
+  });
+
 })();
